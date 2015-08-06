894e62005fc9baf8fdf20000a268594420efea1d
mimikrmvrr <mimi_9204@abv.bg>
2015-08-07 00:22:53 +0100

    Add message details in message view

--- app/src/main/java/com/bigsisters/bigsisters/Question.java
+++ app/src/main/java/com/bigsisters/bigsisters/Question.java
@@ -7,6 +7,33 @@ public class Question {
     private String title;
     private String about;
     private String time;
+    private String text;
+    private String from;
+    private String answer;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 
     public String getTitle() {
         return title;
