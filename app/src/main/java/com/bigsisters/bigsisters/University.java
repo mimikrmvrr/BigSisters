815e9ef9c37e1d56f7b737d2cd85e9880d4b7bcb
stefaniab <=>
2015-08-06 15:44:30 +0100

    Contains a University class and the basics of the UniversityActivity. Firebase links have also been added.

---
+++ app/src/main/java/com/bigsisters/bigsisters/University.java
@@ -0,0 +1,46 @@
+package com.bigsisters.bigsisters;
+
+/**
+ * Created by Stefania on 8/6/15.
+ */
+public class University {
+    int id;
+    String name;
+    String location;
+    String websiteUrl;
+    String photoUrl;
+
+    // get and set methods
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getLocation() {
+        return location;
+    }
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public void setWebsiteUrl(String websiteUrl) {
+        this.websiteUrl = websiteUrl;
+    }
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+}
---
+++ app/src/main/java/com/bigsisters/bigsisters/UniversityActivity.java
@@ -0,0 +1,53 @@
+package com.bigsisters.bigsisters;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.firebase.client.Firebase;
+
+/*
+* Displays info on university
+* Started by an intent that includes the univeristy ID
+* */
+public class UniversityActivity extends ActionBarActivity {
+
+    University uni;
+    static final String EXTRA_ID = "com.bigsister.EXTRA_ID";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
+        setContentView(R.layout.activity_university);
+
+        Intent i = getIntent();
+        int id = Integer.parseInt(i.getStringExtra(UniversityActivity.EXTRA_ID));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_university, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
