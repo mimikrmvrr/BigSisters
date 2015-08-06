b543977a05747d666522e3cab38d65da65af09b5
mimikrmvrr <mimi_9204@abv.bg>
2015-08-06 16:57:00 +0100

    Add News Feed

---
+++ app/src/main/java/com/bigsisters/bigsisters/HomeActivity.java
@@ -0,0 +1,157 @@
+package com.bigsisters.bigsisters;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by demouser on 8/6/15.
+ */
+public class HomeActivity extends ActionBarActivity {
+
+    public class PostsAdapter extends BaseAdapter {
+
+        private class ViewHolder {
+            public TextView name;
+            public TextView time;
+            public ImageView pic;
+            public TextView post_content;
+        }
+
+        private List<Post> posts;
+
+        @Override
+        public int getCount() {
+            return posts.size();
+        }
+
+        @Override
+        public Post getItem(int position) {
+            return posts.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public LinearLayout getView(int position, View convertView, ViewGroup parent) {
+            Log.d("mimi", "getView");
+            LinearLayout postLayout;
+            RelativeLayout postHeaderLayout;
+            ViewHolder viewHolder;
+
+            if (convertView != null) {
+                Log.d("mimi", "convert view");
+                postLayout = (LinearLayout) convertView;
+                viewHolder = (ViewHolder) postLayout.getTag();
+            } else {
+                LayoutInflater postInflater = LayoutInflater.from(HomeActivity.this);
+                postLayout = (LinearLayout) postInflater.inflate(R.layout.post, null);
+              //  LayoutInflater postHeaderInflater = LayoutInflater.from(HomeActivity.this);
+                postHeaderLayout = (RelativeLayout) postLayout.findViewById(R.id.post_header); //postInflater.inflate(R.layout.post_header, null);
+
+                //create ViewHolder
+                viewHolder = new ViewHolder();
+                viewHolder.name = (TextView) postHeaderLayout.findViewById(R.id.name);
+                viewHolder.time = (TextView) postHeaderLayout.findViewById(R.id.time);
+                viewHolder.pic = (ImageView) postHeaderLayout.findViewById(R.id.pic);
+                viewHolder.post_content = (TextView) postLayout.findViewById(R.id.post_content);
+                postLayout.setTag(viewHolder);
+                Log.d("mimi", "create new view");
+            }
+
+            Log.d("mimi", "set text");
+            Post post = getItem(position);
+            if (viewHolder.name != null) {
+                viewHolder.name.setText(post.getName());
+            };
+            viewHolder.time.setText(post.getTime());
+            viewHolder.post_content.setText(post.getText());
+            Picasso.with(getApplicationContext()).load(post.getPicUrl()).into(viewHolder.pic);
+            Log.d("mimi", "everything is ok");
+            return postLayout;
+
+        }
+
+        public void setPosts(List<Post> posts) {
+            this.posts = posts;
+        }
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Firebase.setAndroidContext(this);
+        final Firebase postsRef = new Firebase("https://blazing-torch-4222.firebaseio.com/Posts");
+
+        final ListView list = (ListView) findViewById(R.id.posts);
+
+        postsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                ArrayList<Post> posts = new ArrayList<>();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    posts.add(child.getValue(Post.class));
+                }
+
+                PostsAdapter adapter = new PostsAdapter();
+                adapter.setPosts(posts);
+                list.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
