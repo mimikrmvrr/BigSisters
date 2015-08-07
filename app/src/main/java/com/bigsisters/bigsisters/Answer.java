a23e6bd7317be6b6af8fe57e7c6ac83b7385a607
mimikrmvrr <mimi_9204@abv.bg>
2015-08-07 02:19:53 +0100

    add list of answers to question structure

---
+++ app/src/main/java/com/bigsisters/bigsisters/Answer.java
@@ -0,0 +1,34 @@
+package com.bigsisters.bigsisters;
+
+/**
+ * Created by demouser on 8/7/15.
+ */
+public class Answer {
+    private String text;
+    private String from;
+    private String time;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}
--- app/src/main/java/com/bigsisters/bigsisters/QuestionActivity.java
+++ app/src/main/java/com/bigsisters/bigsisters/QuestionActivity.java
@@ -1,10 +1,19 @@
 package com.bigsisters.bigsisters;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.firebase.client.DataSnapshot;
@@ -15,11 +24,71 @@ import com.firebase.client.ValueEventListener;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by demouser on 8/6/15.
  */
 public class QuestionActivity extends ActionBarActivity {
+    public class AnswerAdapter extends BaseAdapter {
+
+        private class ViewHolder {
+            public TextView text;
+            public TextView time;
+            public TextView from;
+        }
+
+        private List<Answer> answers;
+
+        @Override
+        public int getCount() {
+            return answers.size();
+        }
+
+        @Override
+        public Answer getItem(int position) {
+            return answers.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public LinearLayout getView(int position, View convertView, ViewGroup parent) {
+            LinearLayout layout;
+            ViewHolder viewHolder;
+
+            if (convertView != null) {
+                layout = (LinearLayout) convertView;
+                viewHolder = (ViewHolder) layout.getTag();
+            } else {
+                LayoutInflater postInflater = LayoutInflater.from(QuestionActivity.this);
+                layout = (LinearLayout) postInflater.inflate(R.layout.answ, null);
+
+                //create ViewHolder
+                viewHolder = new ViewHolder();
+                viewHolder.text = (TextView) layout.findViewById(R.id.text);
+                viewHolder.time = (TextView) layout.findViewById(R.id.time);
+                viewHolder.from = (TextView) layout.findViewById(R.id.from);
+                layout.setTag(viewHolder);
+            }
+
+            Answer answer = getItem(position);
+            viewHolder.text.setText(answer.getText());
+            viewHolder.time.setText(answer.getTime());
+            viewHolder.from.setText(answer.getFrom());
+
+            return layout;
+        }
+
+        public void setAnswers(List<Answer> answers) {
+            this.answers = answers;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +98,7 @@ public class QuestionActivity extends ActionBarActivity {
         final TextView about = (TextView) findViewById(R.id.about);
         final TextView text = (TextView) findViewById(R.id.text);
         final TextView from = (TextView) findViewById(R.id.from);
-        final TextView answer = (TextView) findViewById(R.id.answer);
+        final ListView answer = (ListView) findViewById(R.id.answer);
         final TextView time = (TextView) findViewById(R.id.time);
 
         Firebase.setAndroidContext(QuestionActivity.this);
@@ -42,8 +111,12 @@ public class QuestionActivity extends ActionBarActivity {
                 about.setText(question.getAbout());
                 text.setText(question.getText());
                 from.setText(question.getFrom());
-                answer.setText(question.getAnswer());
+                //answer.setText(question.getAnswer());
                 time.setText(question.getTime());
+
+                AnswerAdapter adapter = new AnswerAdapter();
+                adapter.setAnswers(question.getAnswer());
+                answer.setAdapter(adapter);
             }
 
             @Override
